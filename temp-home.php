e97b47ed27e62b32849528a5b75debd6901f2015
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2025-12-04 18:03:37 +0500

    Artist Section

--- temp-home.php
+++ temp-home.php
@@ -175,7 +175,6 @@
         </div>
     </div>
 
-
     <!-- card  -->
     <div class="features-section container">
         <div class="feature-box">
@@ -207,5 +206,128 @@
         </div>
     </div>
 
+    <!-- Artist Spotlight -->
+    <section class="vm-cta vm_artist" style="
+    background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/bwp.png');">
+        <div class="container mt-5 pt-4">
+            <div class="row align-items-center">
+                <!-- LEFT SIDE -->
+                <div class="col-md-6 mb-4 mb-md-0">
+                    <h2 class="immortalize-title">
+                        Artist Spotlight
+                    </h2>
+
+                    <p class="vm-hero-text mt-3">
+                        Viet Chu is an artist who chose photography as one of the mediums for his exploration and
+                        expression of esoteric beauty. In his dedication of that pursuit, he infuses intuitive
+                        sensibility and emotional intelligence to stop and capture a moment of time, that tells a story
+                        for the rest of time.
+                    </p>
+
+                    <a href="#" class="btn vm-hero-btn mt-3 px-4 py-3">
+                        See More ↗
+                    </a>
+                </div>
+                <!-- RIGHT SIDE -->
+                <div class="col-md-6">
+                    <div class="artist-image" style="
+    background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/artist-bg.png');">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/artist.png" width="633"
+                            height="830">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-5 pt-4">
+            <div class="row vm_artist_gallery align-items-end">
+                <div class="col-md-3 work_card">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/work1.png" width="418"
+                        height="574">
+                    <div>
+                        <h6><span>Dune</span> <span>$38.50</span></h6>
+                        <p>16” - 20” Multiple Size</p>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <h2 class="immortalize-title mb-4">
+                        Artist Products
+                    </h2>
+                    <div class="row">
+                        <div class="col-md-4 work_card">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/work2.png" width="418"
+                                height="574">
+                            <div>
+                                <h6><span>Dune</span> <span>$38.50</span></h6>
+                                <p>16” - 20” <span>Multiple Size</span></p>
+                            </div>
+                        </div>
+                        <div class="col-md-4 work_card">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/work3.png" width="418"
+                                height="574">
+                            <div>
+                                <h6><span>Dune</span> <span>$38.50</span></h6>
+                                <p>16” - 20” <span>Multiple Size</span></p>
+                            </div>
+                        </div>
+                        <div class="col-md-4 work_card">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/work4.png" width="418"
+                                height="574">
+                            <div>
+                                <h6><span>Dune</span> <span>$38.50</span></h6>
+                                <p>16” - 20” <span>Multiple Size</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center mt-5">
+                <a href="#" class="btn vm-hero-btn mt-3 px-4 py-3 ">
+                    See More ↗
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <!-- card  -->
+    <div class="features-section container">
+        <div class="stats_box">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stats1.png" class="feature-icon"
+                alt="">
+            <div>
+                <h4>Same Day Deliver</h4>
+                <p>Get your favorite photos printed and delivered to your doorstep on the very same day. Fast, easy &
+                    hassle-free.</p>
+            </div>
+        </div>
+
+        <div class="stats_box">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stats2.png" class="feature-icon" alt="">
+            <div>
+                <h4>Custom Sizes & Materials</h4>
+                <p>Go beyond standard posters with custom dimensions, premium finishes, mountings for high-impact
+                    displays.
+                </p>
+            </div>
+        </div>
+
+        <div class="stats_box">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stats3.png" class="feature-icon" alt="">
+            <div>
+                <h4>Same Day Print</h4>
+                <p>High-quality printing without the waiting. Quick setup, premium results, same-day pickup or delivery.
+                </p>
+            </div>
+        </div>
+
+        <div class="stats_box">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stats4.png" class="feature-icon" alt="">
+            <div>
+                <h4>Flexible Quantities Options</h4>
+                <p>From single event posters to 1000-unit rollouts, we scale with you and offer agency-friendly
+                    reorders.
+                </p>
+            </div>
+        </div>
+    </div>
 </section>
 <?php get_footer(); ?>
