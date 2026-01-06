d5faed1c06dfc27ed7e4da456eee77f76481445e
Mufaqar <mufaqar@gmail.com>
2026-01-06 18:40:22 +0500

    updated

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
             <article id="post-<?php the_ID(); ?>" <?php post_class('card mb-4'); ?>>
                 <div class="card-body">
                     <h2 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <p class="card-text"><?php the_excerpt(); ?></p>
                     <a href="<?php the_permalink(); ?>" class="btn btn-primary">View Gallery</a>
                 </div>
             </article>
