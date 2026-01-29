2a36ec2ccf789d90833d8b872c6f801ac5726833
Mufaqar <mufaqar@gmail.com>
2026-01-29 20:18:17 +0500

    updated

--- template-parts/author-cat-slider.php
+++ template-parts/author-cat-slider.php
@@ -1,14 +1,14 @@
 <section class="my-5 py-5">
-  <div class="container">
-    <h2 class="gradient-title text-center">
-      Categories
-    </h2>
-  </div>
-  <div class="mt-4">
     <div class="container">
-      <div class="cat-slider p-0">
+        <h2 class="gradient-title text-center">
+            Categories
+        </h2>
+    </div>
+    <div class="mt-4">
+        <div class="container">
+            <div class="cat-slider p-0">
 
-        <?php
+                <?php
         // Get all WooCommerce product categories
         $product_categories = get_terms([
           'taxonomy' => 'product_cat',
@@ -21,53 +21,60 @@
             $thumbnail_id = get_term_meta($category->term_id, 'thumbnail_id', true);
             $image_url = $thumbnail_id ? wp_get_attachment_url($thumbnail_id) : wc_placeholder_img_src();
         ?>
-            <div class="px-2">
-              <div class="rounded-cat">
-                <a href="<?php echo esc_url(get_term_link($category)); ?>">
-                  <img src="<?php echo esc_url($image_url); ?>" class="img-fluid" alt="<?php echo esc_attr($category->name); ?>">
-                </a>
-                <div>
-                  <h6>
-                    <a href="<?php echo esc_url(get_term_link($category)); ?>"><?php echo esc_html($category->name); ?></a>
-                  </h6>
+                <div class="px-2">
+                    <div class="rounded-cat">
+                        <a href="<?php echo esc_url(get_term_link($category)); ?>">
+                            <img src="<?php echo esc_url($image_url); ?>" class="img-fluid"
+                                alt="<?php echo esc_attr($category->name); ?>">
+                        </a>
+                        <div>
+                            <h6>
+                                <a
+                                    href="<?php echo esc_url(get_term_link($category)); ?>"><?php echo esc_html($category->name); ?></a>
+                            </h6>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-        <?php
+                <?php
           endforeach;
         else :
           echo '<p>No categories found.</p>';
         endif;
         ?>
 
-      </div>
+            </div>
+        </div>
     </div>
-  </div>
 </section>
 
 <script>
-  jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
     $('.cat-slider').slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      arrows: true,
-      dots: false,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: { slidesToShow: 3 }
-        },
-        {
-          breakpoint: 992,
-          settings: { slidesToShow: 2 }
-        },
-        {
-          breakpoint: 576,
-          settings: { slidesToShow: 1 }
-        }
-      ]
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
-  });
+});
 </script>
